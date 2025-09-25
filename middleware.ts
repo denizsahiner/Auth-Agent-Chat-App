8360498fc9c253fcdb6806ec3aa759b8bfb4fbd1
denizsahiner <dksahiner@gmail.com>
2025-09-25 12:59:50 +0300

    fix: README and env. fixed

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   let res = NextResponse.next({
     request: req,
-  })
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -13,37 +13,43 @@ export async function middleware(req: NextRequest) {
     {
       cookies: {
         getAll() {
-          return req.cookies.getAll()
+          return req.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => req.cookies.set(name, value))
+          cookiesToSet.forEach(({ name, value }) =>
+            req.cookies.set(name, value)
+          );
           res = NextResponse.next({
             request: req,
-          })
+          });
           cookiesToSet.forEach(({ name, value, options }) =>
             res.cookies.set(name, value, options)
-          )
+          );
         },
       },
     }
-  )
+  );
 
   // Auth check
   const {
     data: { session },
-  } = await supabase.auth.getSession()
+  } = await supabase.auth.getSession();
 
-  if (req.nextUrl.pathname.startsWith('/chat') && !session) {
-    return NextResponse.redirect(new URL('/signin', req.url))
+  if (req.nextUrl.pathname.startsWith("/chat") && !session) {
+    return NextResponse.redirect(new URL("/signin", req.url));
   }
 
-  if ((req.nextUrl.pathname === "/signin" || req.nextUrl.pathname === "/signup") && session) {
-    return NextResponse.redirect(new URL('/chat', req.url))
+  if (
+    (req.nextUrl.pathname === "/signin" ||
+      req.nextUrl.pathname === "/signup") &&
+    session
+  ) {
+    return NextResponse.redirect(new URL("/chat", req.url));
   }
 
-  return res
+  return res;
 }
 
 export const config = {
-  matcher: ['/chat/:path*', '/signin','/signup']
-}
+  matcher: ["/chat/:path*", "/signin", "/signup"],
+};
